45ed89d60f411226c5a30740a0f8549ff3eea346
luolei <meleilei@gmail.com>
2013-05-13 19:56:53 +0800

    update to github

--- index.php
+++ index.php
@@ -1,27 +1,16 @@
 <?php get_header(); ?>
-
 <div id="content" class="home-posts-list">
-
 <div id="inner-content" class="wrap clearfix">
-
 <div id="main" class="first clearfix" role="main">
-
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <!-- 首页瀑布流单独box -->
-
-
 <article id="post-<?php the_ID(); ?>" class="post-in-list" <?php post_class('clearfix'); ?> role="article">
-
 <div class="blog-entry-thumbnail">
 <?php dm_the_thumbnail(); ?>
-
 </div> <!-- 缩略图end-->
 <div class="blog-entry-text"> <!-- 首页博文简介-->
 <header class="entry-header">
-
 <h2 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-
-
 </header> <!-- end article header -->
 <section class="entry-content clearfix">
 <?php if(is_category() || is_archive() || is_home() ) {
@@ -30,7 +19,6 @@ the_excerpt('<span class="read-more">阅读全文</span>');
 the_content('<span class="read-more">阅读全文 &raquo;</span>'); 
 } ?>
 </section> <!-- end article section -->
-
 <footer class="entry-meta">
 <li><span class="icon-font">&#58;</span><?php the_time('Y年 n月 j日') ?></li>
 <li><span class="icon-font">&#343;</span><?php comments_popup_link('暂无评论', '1 评论', '% 评论'); ?></li>
@@ -38,16 +26,9 @@ the_content('<span class="read-more">阅读全文 &raquo;</span>');
 <?php echo get_the_category_list('') ;?>
 </div>
 </footer> <!-- end article footer -->
-
 </div><!-- 首页博文简介 END-->
-
-
 </article> <!-- end article -->
-
 <?php endwhile; ?>
-
-
-
 <?php else : ?>
 
 <article id="post-not-found" class="hentry clearfix">
